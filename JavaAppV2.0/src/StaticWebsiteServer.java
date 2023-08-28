6834cc4ff481da3d209f8504f53e1f9b9969d1c6
Suhail <mir.suhail908@gmail.com>
2023-08-28 11:41:42 +0530

    changes

--- JavaAppV2.0/src/StaticWebsiteServer.java
+++ JavaAppV2.0/src/StaticWebsiteServer.java
@@ -1,34 +1,51 @@
-static class MyHandler implements HttpHandler {
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        String requestPath = exchange.getRequestURI().getPath();
-        Path filePath = Paths.get("resources", requestPath);
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
 
-        // Check if the requested file is within the 'components' or 'public' subdirectories
-        if (requestPath.startsWith("/components/") || requestPath.startsWith("/public/")) {
-            filePath = Paths.get("resources" + requestPath);
-        } else if (!Files.exists(filePath)) {
-            // If file not found, serve 404.html
-            filePath = Paths.get("resources/404.html");
-        }
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-        String contentType = getContentType(filePath);
-
-        byte[] responseBytes = Files.readAllBytes(filePath);
-
-        exchange.getResponseHeaders().set("Content-Type", contentType);
-        exchange.sendResponseHeaders(200, responseBytes.length);
-        OutputStream os = exchange.getResponseBody();
-        os.write(responseBytes);
-        os.close();
+public class StaticWebsiteServer {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.start();
+        System.out.println("Server started on port " + port);
     }
 
-    private String getContentType(Path filePath) {
-        if (filePath.toString().endsWith(".html")) {
-            return "text/html";
-        } else if (filePath.toString().endsWith(".css")) {
-            return "text/css";
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String requestPath = exchange.getRequestURI().getPath();
+            Path filePath = Paths.get("resources").resolve(requestPath);
+
+            if (!Files.exists(filePath)) {
+                filePath = Paths.get("resources/404.html");
+            }
+
+            String contentType = getContentType(filePath);
+
+            byte[] responseBytes = Files.readAllBytes(filePath);
+
+            exchange.getResponseHeaders().set("Content-Type", contentType);
+            exchange.sendResponseHeaders(200, responseBytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(responseBytes);
+            os.close();
         }
-        return "text/plain";
+
+        private String getContentType(Path filePath) {
+            if (filePath.toString().endsWith(".html")) {
+                return "text/html";
+            } else if (filePath.toString().endsWith(".css")) {
+                return "text/css";
+            }
+            return "text/plain";
+        }
     }
 }
